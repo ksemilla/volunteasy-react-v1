c3d1e60b1afabb08a2047b5cce1d290d78165c52
Kevin Semilla <kevinsemilla@gmail.com>
2023-06-16 10:42:36 +1000

    Closing for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import { ThemeProvider, createTheme } from "@mui/material"
-import Router from "./routes"
+// import { ThemeProvider, createTheme } from "@mui/material"
+// import Router from "./routes"
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#8a3bc4",
-    },
-  },
-  typography: {
-    button: {
-      textTransform: "none",
-    },
-  },
-})
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: "#8a3bc4",
+//     },
+//   },
+//   typography: {
+//     button: {
+//       textTransform: "none",
+//     },
+//   },
+// })
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Router />
-    </ThemeProvider>
-  )
+  return <div>Closed at the moment</div>
+  // return (
+  //   <ThemeProvider theme={theme}>
+  //     <Router />
+  //   </ThemeProvider>
+  // )
 }
 
 export default App
